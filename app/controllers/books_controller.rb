0b7c6464c3adaa33d2178487e67470308b910f6b
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-07-04 01:43:14 +0000

    [add]テーブル,homes,bookscontroller,viewファイル,ルーティング

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,23 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
