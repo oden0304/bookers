d512d76629c61894998ace17b380d2cee3382bc6
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-07-06 02:47:00 +0000

    [add]create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,13 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book)
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -20,5 +24,10 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
